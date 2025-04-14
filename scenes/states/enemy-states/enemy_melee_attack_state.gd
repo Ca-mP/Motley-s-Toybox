e3c82482b83dbafef30a355ec2218875aa04e399
CKPatterson1s <CKPatterson1s@capetigers.com>
2025-04-14 08:35:59 -0500

    SkillsUSA state build

--- scenes/states/enemy-states/enemy_melee_attack_state.gd
+++ scenes/states/enemy-states/enemy_melee_attack_state.gd
@@ -4,6 +4,8 @@ class_name EnemyMeleeAttackState
 @export var attack_box: Area2D
 @export var damage: int
 
+var has_hit := false
+
 signal saw_player
 signal no_player
 
@@ -11,18 +13,21 @@ func _ready() -> void:
 	super()
 	set_physics_process(false)
 	animator.animation_finished.connect(on_animation_finished)
+	has_hit = false
 
 func enter_state():
 	super()
 	set_physics_process(true)
 	actor.velocity = Vector2.ZERO
 	animator.play("melee_attack")
+	has_hit = false
 
 func _physics_process(_delta: float) -> void:
 	if attack_box.monitoring:
 		if attack_box.get_overlapping_bodies():
-			if "hit" in attack_box.get_overlapping_bodies()[0]:
+			if "hit" in attack_box.get_overlapping_bodies()[0] and not has_hit:
 				attack_box.get_overlapping_bodies()[0].hit(damage, -actor.pivot.scale.x)
+				has_hit = true
 	
 
 func exit_state():
